96ffb47657ad89f4ed2ef3cce1e612ba077da6a0
Daryl <donamedenou@gmail.com>
2024-11-14 11:54:19 +0100

    Ajout de la fonctionnalité de tri par nom de l'auteur

--- backend/src/main/java/com/example/GestionBibliotheque/Controllers/LivreController.java
+++ backend/src/main/java/com/example/GestionBibliotheque/Controllers/LivreController.java
@@ -2,6 +2,7 @@ package com.example.GestionBibliotheque.Controllers;
 
 import com.example.GestionBibliotheque.Models.Livre;
 import com.example.GestionBibliotheque.Repositories.LivreRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/livre")
+@CrossOrigin()
 public class LivreController {
     private final LivreRepository livreRepository;
 
@@ -20,5 +22,10 @@ public class LivreController {
     @GetMapping
     public List<Livre> getAllLivres(){
         return livreRepository.findAll();
+    }
+
+    @GetMapping("/orderedByAuteur")
+    public List<Livre> getLivresOrderedByAuteur(){
+        return livreRepository.findAllByOrderByAuteurAsc();
     }
 }
